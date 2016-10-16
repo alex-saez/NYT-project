9f5eb224a9e960cb2d5172b79f7e267d674e2366
alex-saez <as3171@columbia.edu>
2016-10-16 09:41:46 -0400

    app update

--- app/server.R
+++ app/server.R
@@ -1,48 +1,58 @@
 library(shiny)
 library(wordcloud)
-load("../data/database_Oct14.RData")
-load("../data/dtm_Oct14.RData")
+load("../data/ind_popular_Oct14.RData")
+load("../data/dtm_unstemmed_Oct14.RData")
+# load("../data/mutual_info_Oct14.RData")
 
+
+# compute word frequency
 terms = dtm$dimnames$Terms[dtm$j]
+word_counts = tapply(dtm$v, terms, sum)
 
 
 shinyServer(function(input, output) {
   
-  # Define a reactive expression for the document term matrix
-  
- pop_score = reactive({
+  # read type of popular
+  pop_score = reactive({
    switch(input$popular, 
-          'ME' = data$most_emailed, 
-          'MV' = data$most_viewed, 
-          'MS' = data$most_shared 
+          'ME' = popular$most_emailed, 
+          'MV' = popular$most_viewed, 
+          'MS' = popular$most_shared 
    )
  }) 
   
   
- top_pop = reactive({
-   # define most-popular
+ # indices for most-popular articles:
+ top_pop_ind = reactive({
    pop = pop_score()
    pop = pop[pop>0]
    which(pop_score() %in% sort(pop, decreasing = TRUE)[1:(input$top_prop*length(pop)/100)])
  })
  
 
-  
-  # Make the wordcloud drawing predictable during a session
+  # make the wordcloud drawing predictable during a session:
   wordcloud_rep <- repeatable(wordcloud)
 
-  output$plot = renderPlot({
+  output$wordcloud = renderPlot({
     
-    # compute word frequency in popular articles
-    word_counts_pop = tapply(dtm$v[dtm$i %in% top_pop()], terms[dtm$i %in% top_pop()], sum)
+    #rel_freq_pop = important_terms(dtm, top_pop_ind(), len=100, method="mi", tail="top")
+      
+    # compute word count in popular articles
+    word_counts_pop = tapply(dtm$v[dtm$i %in% top_pop_ind()], terms[dtm$i %in% top_pop_ind()], sum)
+    non_pop_terms = setdiff(terms, names(word_counts_pop)) # so that # terms is same as in word_counts
+    non_pop_terms = array(0, dim=length(non_pop_terms), dimnames=list(non_pop_terms))
+    word_counts_pop = c(word_counts_pop, non_pop_terms)
+    word_counts_pop = word_counts_pop[order(names(word_counts_pop))]
 
-    wordcloud_rep(names(word_counts_pop), word_counts_pop, scale=c(4,0.5),
-                  min.freq = 1, max.words=100,
-                  colors=brewer.pal(8, "Dark2"))
+    # compare word frequencies between POP and ALL:
+    term_freq = word_counts/sum(word_counts)
+    term_freq_pop = word_counts_pop/sum(word_counts_pop)
+    rel_freq_pop = term_freq_pop - term_freq
+
+    wordcloud_rep(names(rel_freq_pop), rel_freq_pop, scale = c(4,0.5),
+                  max.words = input$num_words,
+                  colors = brewer.pal(9, "Set1"))
   })
   
-  
-  
-  output$print = renderPrint({head(names(word_counts_pop))})
   
 })
--- app/ui.r
+++ app/ui.r
@@ -9,20 +9,27 @@ shinyUI(
   fluidPage(
     
       # Application title
-      titlePanel("Word Analysis"),
+      titlePanel("Over-represented words in popular New York Times articles"),
       
       sidebarLayout(
         # Sidebar with a slider and selection inputs
         sidebarPanel(
           
-          selectInput("popular", "Type of popular article:",
+          selectInput("popular", 
+                      h4("Type of popular article:"),
                       choices = type_popular),
                     
           hr(),
           
           sliderInput("top_prop",
-                      "How popular (define top percentile):",
-                      min = 1,  max = 100, value = 100), 
+                      h4("Top percentile of popular (lower = more popular):"),
+                      min = 1,  max = 100, value = 100),
+          
+          hr(),
+          
+          sliderInput("num_words",
+                      tags$small("Number of words to display:"),
+                       min = 10,  max = 100, value = 30),
           
           submitButton("Update")
           
@@ -30,13 +37,15 @@ shinyUI(
 
         # Show Word Cloud
         mainPanel(
-          plotOutput("plot")
+          plotOutput("wordcloud", width = "100%"), 
+          tags$small("Note: This plot displays words whose frequency in popular articles is much higher that their baseline frequency.
+                     It is based on all articles published in The New York Times from March 15th to October 14th 2016.  The New
+                     York Times compiles three daily lists of popular articles: Mose Emailed, Most Viewed and Most Shared. For each of 
+                     these categories, each article received a score according to its rank within the list and the duration it remained 
+                     on the list. The slider allows to select a top percentile of these scores. ")
         )
         
-        # mainPanel(
-        #   textOutput("print")
-        # )
-        
+
       )
       
       
